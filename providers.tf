e543cbf9fa1a54338725519bf9dd7ac171eb846e
James Bernardi <jamesbernardi@users.noreply.github.com>
2023-03-24 21:46:55 -0400

    first commit

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2"
+    }
+  }
+  required_version = ">= 1.4"
+}
+
+provider "google" {
+  # Configuration options
+}
